bcae4a4584878a1ac6c3df93d5ab7cc7dd51ba2e
Jesse Emana <jesseemana@gmail.com>
2023-11-24 06:51:05 +0200

    CHORE: finished setting up server and created graceful shutdown

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,11 @@ import cors from 'cors'
 import helmet from 'helmet'
 import cookieParser from 'cookie-parser'
 import deserializeuser from './middleware/deserialize-user'
+import database from './utils/connect-db'
+import log from './utils/logger'
 
-// npm i express jwt bcrypt @typegoose/typegoose mongoose helmet dotenv cors cookie-parser express-rate-limit nanoid config pino dayjs
-// npm i --save-dev @types/node @types/pino-pretty ts-node-dev @types/express @types/jwt @types/bcrypt @types/config @types/mongoose @types/helmet @types/cors @types/cookie-parser @types/express-rate-limit @types/nanoid
+// npm i express zod inversify jwt bcrypt @typegoose/typegoose mongoose helmet dotenv cors cookie-parser express-rate-limit nanoid config pino dayjs
+// npm i --save-dev @types/node @types/inversify @types/pino-pretty ts-node-dev @types/express @types/jwt @types/bcrypt @types/config @types/mongoose @types/helmet @types/cors @types/cookie-parser @types/express-rate-limit @types/nanoid
 
 const app = express()
 
@@ -20,5 +22,28 @@ app.use(deserializeuser)
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+const startServer = () => {
+  const server = app.listen(PORT, () => {
+    database.connect()
+    log.info(`App listening on port ${PORT}`)
+  })
 
-app.listen(PORT, () => console.log(`App listening on port ${PORT}`))
+  const signals = ['SIGTERM', 'SIGINT']
+
+  function gracefulShutdown(signal: string) {
+    process.on(signal, () => {
+      log.info(`Received signal: ${signal}, shutting down...`)
+      server.close()
+      database.disconnect()
+      log.info('Goodbye')
+
+      process.exit(0)
+    })
+  }
+
+  for (let i = 0; i < signals.length; i++) {
+    gracefulShutdown(signals[i])
+  }
+}
+
+startServer()
