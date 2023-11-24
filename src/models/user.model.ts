c776adfcdb6a1a8844ec9113babaf850bbc1771d
Jesse Emana <jesseemana@gmail.com>
2023-11-24 06:04:02 +0200

    CHORE: created users' model

---
+++ src/models/user.model.ts
@@ -0,0 +1,43 @@
+import bcrypt from 'bcrypt'
+import config from 'config'
+import log from '../utils/logger'
+import { prop, getModelForClass, DocumentType, pre, modelOptions, Severity, index } from '@typegoose/typegoose'
+
+@pre<User>('save', function() {
+  if (this.isModified('password')) { 
+    const salt = bcrypt.genSaltSync(config.get<number>('saltWorkFactor'))
+    const hash = bcrypt.hashSync(this.password, salt)
+    this.password = hash
+
+    return
+  }
+})
+
+@index({ email: 1})
+
+export class User {
+  @prop({ required: true })
+  first_name: string
+
+  @prop({ required: true })
+  last_name: string
+
+  @prop({ lowercase: true, required:true, unique: true })
+  email: string
+
+  @prop({ required: true })
+  password: string
+
+  verifyPassword(this: DocumentType<User>, candidate_password: string) {
+    try {
+      return bcrypt.compareSync(candidate_password, this.password)
+    } catch(err) {
+      log.error('Failed to validate password')
+      return false
+    }
+  }
+}
+
+const UserModel = getModelForClass(User)
+
+export default UserModel
