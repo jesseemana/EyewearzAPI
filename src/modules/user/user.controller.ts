b45a112fe6168d5c6498e267c007b2e39b76e806
Jesse Emana <jesseemana@gmail.com>
2023-11-25 16:30:39 +0200

    REFACTOR: fixed promise when creating user

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -20,7 +20,7 @@ export class UserController {
   async createUserHandler (req: Request, res: Response) {
     try {
       const data = req.body
-      const new_user = this._userService.registerUser(data)
+      const new_user = await this._userService.registerUser(data)
       res.status(200).send(`New user ${new_user.first_name} ${new_user.last_name} has been created.`)
     } catch (error: any) {
       if (error.code === 11000) {
