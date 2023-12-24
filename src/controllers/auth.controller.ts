364407e8632f555737d7430f77f4ed58edc0bace
Jesse Emana <jesseemana@gmail.com>
2023-12-24 08:52:49 +0200

    REFACTOR: modified status code for when user has loged out

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,9 +2,11 @@ import { Request, Response } from 'express'
 import AuthService from '../services/auth.service'
 import { LoginInput, login_schema } from '../schema/user'
 
-export async function login(req: Request<{}, {}, LoginInput>, res: Response) {
+export async function login(
+  req: Request<{}, {}, LoginInput>, 
+  res: Response
+) {
   const { email, password } = login_schema.parse(req.body)
-    
   try {
     const user = await AuthService.findUserByEmail(email)
     if (!user || !user.verifyPassword(password))
@@ -19,16 +21,16 @@ export async function login(req: Request<{}, {}, LoginInput>, res: Response) {
   }
 }
 
-export async function logout(req: Request, res: Response) {
+export async function logout(_: Request, res: Response) {
   try {
     const session_id = res.locals.user.session._id as string
     const session = await AuthService.findSessionById(session_id)
     if (!session || !session.valid) 
-      return res.status(401).send('Session is not found or is invalid')
+      return res.status(401).send('Session not found or is invalid')
 
     await AuthService.destroySession({ _id: session_id }, { valid: false })
 
-    res.send('User loged out successfully.')
+    res.status(200).send('User loged out successfully.')
   } catch (error) {
     return res.status(500).send('Internal server error!')
   }
