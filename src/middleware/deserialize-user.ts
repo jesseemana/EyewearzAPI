37674e66e43eb47ea4c35cf310c211d6dc4240bb
Jesse Emana <jesseemana@gmail.com>
2023-12-25 23:08:05 +0200

    REFACTOR: changed return type for verifyToken function

--- src/middleware/deserialize-user.ts
+++ src/middleware/deserialize-user.ts
@@ -4,10 +4,11 @@ import { verifyToken } from '../utils/jwt'
 const deserialize_user = (req: Request, res: Response, next: NextFunction) => {
   if (req.headers && req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
     const token = req.headers.authorization.split(' ')[1].trim()
-    const decoded = verifyToken<string>(token, 'accessTokenPublicKey')
+    const decoded = verifyToken(token, 'accessTokenPublicKey')
     if (decoded) { res.locals.user = decoded }
     return next()
   }
+
   return next()
 }
 
