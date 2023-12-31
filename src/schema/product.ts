06e9fd5b810c232ba41f83b8782a8dfbac4d7ef5
Jesse Emana <jesseemana@gmail.com>
2023-12-31 21:01:07 +0200

    REFACTOR: modified shema types

--- src/schema/product.ts
+++ src/schema/product.ts
@@ -1,13 +1,20 @@
 import { string, object, TypeOf, z } from 'zod'
 
 export const product_schema = object({
-  name: string({ required_error: 'Name id is required' }).trim(),
-  price: string({ required_error: 'Image id is required' }).trim(),
-  category: z.enum(['sunglasses', 'prescription', 'blulelight', 'fashion']),
-  gender: z.enum(['male', 'female', 'kids']),
+  name: string({ 
+    required_error: 'Name is required' 
+  }).trim(),
+  price: string({ 
+    required_error: 'Image is required' 
+  }).trim(),
+  category: z.enum(['sunglasses', 'prescription', 'bluelight', 'kids', 'unisex']),
+  gender: z.enum(['male', 'female', 'kids', 'unisex']),
   description: string({ 
     required_error: 'Product description is required' 
-  }).trim().min(10, 'Minimum of 10 characters').max(300, 'Description cannot be longer than 64 characters'),
+  })
+  .trim()
+  .min(10, 'Minimum of 10 characters')
+  .max(300, 'Description cannot be longer than 64 characters'),
 })
 
 export type ProductInput = TypeOf<typeof product_schema>
