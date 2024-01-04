9f11bccd152efc05fc04ebeb598aa775ca35111b
Jesse Emana <jesseemana@gmail.com>
2024-01-04 23:18:37 +0200

    CHORE: changed to arrow functions

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,49 +1,52 @@
-import CustomerModel from '../models/customer.model'
 import { omit } from 'lodash'
 import { signJwt } from '../utils/jwt'
-import { DocumentType } from '@typegoose/typegoose'
-import { Customer, private_fields } from '../models/customer.model'
 import { FilterQuery, UpdateQuery } from 'mongoose'
+import { DocumentType } from '@typegoose/typegoose'
 import SessionModel, { Session } from '../models/session.model'
+import CustomerModel, { Customer, private_fields } from '../models/customer.model'
 
-async function findUserByEmail(email: string) {
+const findUserByEmail = async (email: string) => {
   return CustomerModel.findOne({ email })
 }
 
-async function findUserById(id: string) {
+const findUserById = async (id: string) => {
   return CustomerModel.findById(id)
 }
 
-async function createSession({ user_id }: { user_id: string }) {
+const createSession = async ({ user_id }: { user_id: string }) => {
   return SessionModel.create({ user_id })
 }
 
-async function findSessionById(id: string) {
+const findSessionById = async (id: string) => {
   return SessionModel.findById(id)
 }
 
-function signAccessToken(
+const signAccessToken = (
   user: DocumentType<Customer>, 
   session: DocumentType<Session>
-) {
+) => {
   const user_payload = omit(user.toJSON(), private_fields)
-  const access_token = signJwt({ ...user_payload, session }, 'accessTokenPrivateKey', { expiresIn: '15m' })
+  const access_token = signJwt(
+    { ...user_payload, session }, 
+    'accessTokenPrivateKey', 
+    { expiresIn: '15m' }
+  )
 
   return access_token 
 }
 
-async function destroySession(
+const destroySession = async (
   filter: FilterQuery<Session>, 
   update: UpdateQuery<Session>
-) {
+) => {
   return SessionModel.updateOne(filter, update)
 }
 
 export default {
-  findUserByEmail, 
   findUserById, 
   createSession, 
-  signAccessToken, 
   destroySession,
-  findSessionById
+  signAccessToken, 
+  findSessionById,
+  findUserByEmail, 
 }
