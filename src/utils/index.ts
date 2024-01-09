3fd3c0b3e48898e78c596d1f37e8acf381ca4dc1
Jesse Emana <jesseemana@gmail.com>
2024-01-09 09:36:13 +0200

    CHORE: created swagger docs setup

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,11 +1,13 @@
 import jwt from './jwt'
-import logger from './logger'
+import log from './logger'
 import database from './connect-db'
+import swaggerDocs from './swagger'
 import uploadPicture from './upload-picture'
 
 export {
   jwt, 
-  logger, 
+  log, 
   database, 
+  swaggerDocs,
   uploadPicture
 }
