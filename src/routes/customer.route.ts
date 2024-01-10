a25591abdeee92ebc20e9dae1529c95c4565d4cc
Jesse Emana <jesseemana@gmail.com>
2024-01-10 22:37:11 +0200

    CHORE: swagger docs

--- src/routes/customer.route.ts
+++ src/routes/customer.route.ts
@@ -10,37 +10,37 @@ const router = Router()
  * @openapi
  * '/api/users':
  *  post:
- *    tags:
+ *      tags:
  *      - Register User/Customer
- *    summary: Resgister a user/customer
- *    requestBody:
- *      required: true
- *      content:
- *        application/json
- *          schema:
- *            $ref: '#/components/schemas/create_user_input'
- *    responses:
- *      201:
- *        description: Created
+ *      summary: Resgister a user/customer
+ *      requestBody:
+ *        required: true
  *        content:
  *          application/json:
  *            schema:
- *              $ref: '#/components/schemas/create_user_response'
- *      409:
- *        description: Conflict
- *      400:
- *        description: Bad request
- *      500:
- *        description: Internal server error
- * get:
- *    tags:
- *      - Get Users/Customers
- *    summary: Get all users/customers
- *    responses:
+ *              $ref: '#/components/schemas/CreateUserInput'
+ *      responses:
+ *        201:
+ *          description: Created
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/CreateUserResponse'
+ *        409:
+ *          description: Conflict
+ *        400:
+ *          description: Bad request
+ *        500:
+ *          description: Internal server error
+ *  get:
+ *     tags:
+ *     - Get Users/Customers
+ *     summary: Get all users/customers
+ *     responses:
  *        200:
- *         description: Get all users
+ *          description: Get all users
  *        404:
- *         description: No users found
+ *          description: No users found
  */
 router.route('/')
   .get(getAllCustomers)
