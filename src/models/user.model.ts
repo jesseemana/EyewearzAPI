06c19c186b8e41a56bfd80ba5b3528d12013813d
Jesse Emana <jesseemana@gmail.com>
2024-01-11 14:29:45 +0200

    CHORE: User model

---
+++ src/models/user.model.ts
@@ -0,0 +1,66 @@
+import argon2 from "argon2"
+import log from '../utils/logger'
+import { 
+  pre, 
+  prop, 
+  index,
+  Severity, 
+  DocumentType, 
+  modelOptions, 
+  getModelForClass, 
+} from '@typegoose/typegoose'
+
+export const private_fields = ['cart', 'favorites', 'password']
+
+@pre<User>('save', async function() {
+  if (this.isModified('password')) { 
+    const hash = await argon2.hash(this.password)
+    this.password = hash
+    return
+  }
+  return
+})
+
+@index({ email: 1})
+
+@modelOptions({
+  options: {
+    allowMixed: Severity.ALLOW
+  }
+})
+
+export class User {
+  @prop({ required: true })
+  first_name: string
+
+  @prop({ required: true })
+  last_name: string
+
+  @prop({ lowercase: true, required: true, unique: true })
+  email: string
+
+  @prop({ required: true, default: 'user' })
+  role: string
+
+  @prop({ required: true })
+  password: string
+
+  @prop({ default: [] })
+  cart: Array<string>
+
+  @prop({ default: [] })
+  favorites: Array<string>
+
+  async verifyPassword(this: DocumentType<User>, candidate_password: string) {
+    try {
+      return await argon2.verify(this.password, candidate_password)
+    } catch(err) {
+      log.error(err, 'Failed to validate password')
+      return false
+    }
+  }
+}
+
+const UserModel = getModelForClass(User)
+
+export default UserModel
