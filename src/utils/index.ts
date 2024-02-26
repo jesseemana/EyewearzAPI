f9bea051619673c886c298a2ba83b46deaf316e2
Jesse Emana <jesseemana@gmail.com>
2024-02-26 02:04:33 +0200

    REFACTOR: utility function for generating reset code

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,6 +2,7 @@ import log from './logger'
 import { Database } from './database'
 import swaggerDocs from './swagger'
 import { signJwt, verifyToken } from './jwt'
+import { generate_code } from './generate-code'
 import uploadPicture from './upload-picture'
 
 export {
@@ -9,6 +10,7 @@ export {
   Database, 
   signJwt, 
   verifyToken, 
+  generate_code,
   swaggerDocs,
   uploadPicture
 }
