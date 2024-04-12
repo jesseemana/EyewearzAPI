21dab8b29ca38a59c3fe90ed6ba3ed0786fb1918
Jesse Emana <jesseemana@gmail.com>
2024-04-12 22:32:58 +0200

    REFACTOR: removed swagger docs

--- src/schema/product.ts
+++ src/schema/product.ts
@@ -1,87 +1,6 @@
-import { string, object, TypeOf, z } from 'zod'
+import { string, object, TypeOf, z } from 'zod';
 
-/**
- * @openapi
- * components:
- *  schema:
- *    GetProducts:
- *      type: array
- *      items:
- *        type: object
- *        required:
- *          - name
- *          - price
- *          - category
- *          - gender
- *          - image
- *          - cloudinary_id
- *          - description
- *        properties:
- *          _id:
- *            type: string
- *          name:
- *            type: string
- *          price:
- *            type: string
- *          category:
- *            type: string
- *          gender:
- *            type: string
- *          image:
- *            type: string
- *          cloudinary_id:
- *            type: string
- *          description:
- *            type: string
- *          __v:
- *            type: number
- *    CreateProductInput:
- *      type: object
- *      required:
- *        - name
- *        - price
- *        - category
- *        - gender
- *        - description
- *      properties:
- *        name:
- *          type: string
- *          default: sunglasses
- *        price:
- *          type: string
- *          default: 30000
- *        category:
- *          type: string
- *          default: sunglasses
- *        gender:
- *          type: string
- *          default: male
- *        description:
- *          type: string
- *          default: designed for first-time DSLR owners who want impressive results straight out of the box. With easy to use automatic shooting modes, large 24.1 MP sensor, Canon Camera Connect app integration and built-in feature guide, EOS 1500D is always ready to go.
- *    productResponse:
- *      type: object
- *      properties:
- *        _id:
- *          type: string
- *        name:
- *          type: string
- *        price:
- *          type: string
- *        category:
- *          type: string
- *        gender:
- *          type: string
- *        image:
- *          type: string
- *        cloudinary_id:
- *          type: string
- *        description:
- *          type: string
- *        __v:
- *          type: number
- */
-const product_schema = object({
+const productSchema = object({
   name: string({ 
     required_error: 'Name is required' 
   }).trim(),
@@ -92,12 +11,9 @@ const product_schema = object({
   gender: z.enum(['male', 'female', 'unisex']),
   description: string({ 
     required_error: 'Product description is required' 
-  })
-  .trim()
-  .min(10, 'Minimum of 10 characters')
-  .max(300, 'Description cannot be longer than 64 characters'),
-})
+  }).trim().min(10, 'Minimum of 10 characters').max(300, 'Description cannot be longer than 64 characters'),
+});
 
-export type ProductInput = TypeOf<typeof product_schema>
+export type ProductInput = TypeOf<typeof productSchema>;
 
-export default product_schema
+export default productSchema;
--- src/schema/user.ts
+++ src/schema/user.ts
@@ -1,85 +1,6 @@
-import { string, object, TypeOf, z } from 'zod'
+import { string, object, TypeOf, z } from 'zod';
 
-/**
- * @openapi
- * components:
- *  schema:
- *    getAllUsers:
- *      type: array
- *      items:
- *        type: object
- *        required:
- *          - first_name
- *          - last_name
- *          - email
- *          - role
- *          - cart
- *          - favorites
- *        properties:
- *          _id:
- *            type: string
- *          first_name:
- *            type: string
- *          last_name:
- *            type: string
- *          email:
- *            type: string
- *          role:
- *            type: string
- *          cart:
- *            type: array
- *            items:
- *              type: string
- *          favorites:
- *            type: array
- *            items:
- *              type: string
- *          __v:
- *            type: number
- *    CreateUserInput:
- *      type: object
- *      required:
- *        - first_name
- *        - last_name
- *        - email
- *        - role
- *        - password
- *      properties:
- *        first_name:
- *          type: string
- *          default: John
- *        last_name:
- *          type: string
- *          default: Doe
- *        email:
- *          type: string
- *          default: johndoe@example.com
- *        role:
- *          type: string
- *          default: admin
- *        password:
- *          type: string
- *          default: Password_1234
- *        confirm_password:
- *          type: string
- *          default: Password_1234
- *    userResponse:
- *      type: object
- *      properties:
- *         _id:
- *          type: string
- *         first_name:
- *          type: string
- *         last_name:
- *          type: string
- *         email:
- *          type: string
- *         role:
- *          type: string
- *         __v:
- *          type: number
- */
-export const user_schema = object({
+export const userSchema = object({
   first_name: string({
     required_error: 'First name is required'
   }).trim(),
@@ -92,91 +13,47 @@ export const user_schema = object({
   role: z.enum(['admin', 'user']),
   password: string({
     required_error: 'Password is required',
-  }).regex(new RegExp(".*[A-Z].*"), "One uppercase character").regex(new RegExp(".*[a-z].*"), "One lowercase character")
-    .regex(new RegExp(".*\\d.*"), "One number")
-    .regex(new RegExp(".*[`~<>?,./!@#$%^&*()\\-_+=\"'|{}\\[\\];:\\\\].*"),"One special character")
-    .min(8, "Must not be less than 8 characters.")
-    .max(64, "Cannot be more than 64 characters long."),
+  }).min(8, "Must not be less than 8 characters.").max(24, "Cannot be more than 24 characters long."),
   confirm_password: string({
     required_error: 'Password confirmation is required',
-  }).regex(new RegExp(".*[A-Z].*"), "One uppercase character").regex(new RegExp(".*\\d.*"), "One number")
-    .regex(new RegExp(".*[a-z].*"), "One lowercase character")
-    .regex(new RegExp(".*[`~<>?,./!@#$%^&*()\\-_+=\"'|{}\\[\\];:\\\\].*"),"One special character")
-    .min(8, "Must not be less than 8 characters.")
-    .max(64, "Cannot be more than 64 characters long."),
+  }).min(8, "Must not be less than 8 characters.").max(24, "Cannot be more than 24 characters long."),
 }).refine((data) => data.password === data.confirm_password, {
   message: 'Passwords do not match',
   path: ['confirm_password'],
-})
+});
 
-
-/**
- * @openapi
- * components:
- *  schema:
- *    loginUserInput:
- *      type: object
- *      required:
- *        - email
- *        - password
- *      properties:
- *        email:
- *          type: string
- *          default: johndoe@example.com
- *        password:
- *          type: string
- *          default: Password_1234
- *    loginUserResponse:
- *      type: object
- *      required:
- *        - accessToken
- *      properties:
- *        accessToken:
- *          type: string 
- */
-export const login_schema = object({
+export const loginSchema = object({
   email: string({ required_error: 'Email is required' }).email('Enter a valid email').trim().toLowerCase(),
   password: string({
     required_error: 'Password is required',
-  }).regex(new RegExp(".*[A-Z].*"), "One uppercase character").regex(new RegExp(".*[a-z].*"), "One lowercase character")
-    .regex(new RegExp(".*\\d.*"), "One number")
-    .regex(new RegExp(".*[`~<>?,./!@#$%^&*()\\-_+=\"'|{}\\[\\];:\\\\].*"),"One special character")
-    .min(8, "Must not be less than 8 characters.")
-    .max(64, "Cannot be more than 64 characters long."),
-})
+  }).min(8, "Must not be less than 8 characters.").max(24, "Cannot be more than 24 characters long."),
+});
 
-export const forgot_schema = object({
+export const forgotSchema = object({
   email: string({ required_error: 'Email is required' }).email('Enter a valid email').trim().toLowerCase(),
-})
+});
 
-
-export const reset_schema = object({
+export const resetSchema = object({
   params: object({
     id: string({ required_error: 'Id is required' }).trim(),
-    password_reset_code: string({ required_error: 'Password reset code is required' }).trim(),
+    reset_code: string({ required_error: 'Password reset code is required' }).trim(),
   }),
   body: object({
     password: string({
       required_error: 'Please provide a password'
-    }).regex(new RegExp(".*[A-Z].*"), "One uppercase character").regex(new RegExp(".*\\d.*"), "One number")
-      .regex(new RegExp(".*[a-z].*"), "One lowercase character")
-      .regex(new RegExp(".*[`~<>?,./!@#$%^&*()\\-_+=\"'|{}\\[\\];:\\\\].*"),"One special character")
-      .min(8, "Must not be less than 8 characters.")
-      .max(64, "Cannot be more than 64 characters long."),
+    }).min(8, "Must not be less than 8 characters.")
+      .max(24, "Cannot be more than 24 characters long."),
     confirm_password: string({
       required_error: 'Please confirm your password'
-    }).regex(new RegExp(".*[A-Z].*"), "One uppercase character").regex(new RegExp(".*\\d.*"), "One number")
-      .regex(new RegExp(".*[a-z].*"), "One lowercase character")
-      .regex(new RegExp(".*[`~<>?,./!@#$%^&*()\\-_+=\"'|{}\\[\\];:\\\\].*"),"One special character")
-      .min(8, "Must not be less than 8 characters.")
-      .max(64, "Cannot be more than 64 characters long."),
+    }).min(8, "Must not be less than 8 characters.")
+      .max(24, "Cannot be more than 24 characters long."),
   }).refine((data) => data.password === data.confirm_password, {
     message: 'Passwords do not match',
     path: ['confirm_password'],
   }),
-})
+});
 
-export type UserInput = TypeOf<typeof user_schema>
-export type LoginInput = TypeOf<typeof login_schema>
-export type ForgotInput = TypeOf<typeof forgot_schema>
-export type ResetInput = TypeOf<typeof reset_schema>
+export type UserInput = TypeOf<typeof userSchema>;
+export type LoginInput = TypeOf<typeof loginSchema>;
+export type ResetInput = TypeOf<typeof resetSchema>;
+export type ForgotInput = TypeOf<typeof forgotSchema>;
