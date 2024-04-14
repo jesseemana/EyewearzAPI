0dbc2c7ac4d9fe3cc0ae1feb28bfaa94b091762c
Jesse Emana <jesseemana@gmail.com>
2024-04-15 01:19:04 +0200

    CHORE: added featuerd route

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,15 +1,17 @@
 import { Router } from 'express';
-import { require_admin, upload } from '../middleware';
+import { requireAdmin, upload } from '../middleware';
 import { ProductController } from '../controllers';
 
 const router = Router();
 
 router.get('/', ProductController.getAllProductsHandler);
 
+router.get('/featured', ProductController.getFeaturedProductsHandler);
+
 router.post('/filter/:query', ProductController.searchProductsHandler);
 
 router.get('/:id', ProductController.getSingleProductHandlerandler);
 
-router.post('/create', [upload.single('file'), require_admin], ProductController.createProductHandlerandler);
+router.post('/create', [upload.single('file'), requireAdmin], ProductController.createProductHandlerandler);
 
 export default router;
