2c1574245b398b4e94c72352ffc2c963264a19a0
Jesse Emana <jesseemana@gmail.com>
2024-04-17 02:02:23 +0200

    REFACTOR: fixed typos

--- src/schema/booking.ts
+++ src/schema/booking.ts
@@ -1,12 +1,12 @@
 import { string, object, TypeOf, date } from 'zod'
 
-const create_reservation = object({
+const reservationSchema = object({
   phone: string({ required_error: 'Provide your phone number' }).trim(),
   reason: string({ required_error: 'Provide reason for booking' }).trim(),
   location: string({ required_error: 'Provide location' }).trim(),
   date: date()
 })
 
-export type BookingInput = TypeOf<typeof create_reservation>
+export type BookingInput = TypeOf<typeof reservationSchema>
 
-export default create_reservation
+export default reservationSchema
--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,10 +1,10 @@
-import { user_schema, login_schema } from './user'
-import booking_schema from './booking'
-import product_schema from './product'
+import { userSchema, loginSchema } from './user'
+import bookingSchema from './booking'
+import productSchema from './product'
 
 export {
-  user_schema,
-  login_schema,
-  booking_schema,
-  product_schema
+  userSchema,
+  loginSchema,
+  bookingSchema,
+  productSchema
 }
