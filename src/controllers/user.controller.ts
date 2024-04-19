dba0a465635cf19580df36439f55cf94306c93a7
Jesse Emana <jesseemana@gmail.com>
2024-04-19 12:12:59 +0200

    CHORE: created user update

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,81 +1,145 @@
-import { Request, RequestHandler, Response } from 'express'
-import { ForgotInput, ResetInput, UserInput } from '../schema/user'
-import { UserService }from '../services'
-import { log } from '../utils'
+import { Request, Response } from 'express';
+import { ResetInput, UpdateUserInput, UserInput } from '../schema/user';
+import { UserService }from '../services';
+import { generateCode, log, sendEmail } from '../utils';
 
-const BASE_URL = 'http://localhost:3030/api/v1/user'
 
-const get_users_handler: RequestHandler = async (_req: Request, res: Response) => {
+const getCurrentUserHandler = async (_req: Request, res: Response) => {
   try {
-    const all_users = await UserService.get_all_users()
-    return res.status(200).send(all_users)
+    const user = res.locals.user;
+    if (!user) return res.status(404).send('No user found.');
+    log.info({ user });
+    return res.status(200).send(user);
   } catch(error) {
-    return res.status(500).send('Internal Server Error')
+    return res.status(500).send('Internal Server Error.');
   }
 }
 
-const create_user_handler: RequestHandler = async (
+
+async function createUserHandler(
   req: Request<{}, {}, UserInput>, 
   res: Response
-) => {
+) {
   try {
-    const data = req.body
-    const new_user = await UserService.register_user(data)
-    return res.status(201).send(`New user ${new_user.first_name} ${new_user.last_name} has been registered.`)
+    const data = req.body;
+    const user = await UserService.registerUser(data);
+    return res.status(201).send(`New user ${user.first_name} ${user.last_name} has been registered.`);
   } catch (error: any) {
-    if (error.code === 11000) 
-      return res.send('Account already exists')
-    return res.status(500).send('Internal Server Error')
+    if (error.code === 11000) {
+      return res.status(400).send('Email already in use.');
+    }
+    return res.status(500).send('Internal Server Error.');
   }
 }
 
-const forgot_password_handler: RequestHandler = async (
-  req: Request<{}, {}, ForgotInput>, 
+
+async function updateUserHandler(
+  req: Request<UpdateUserInput['params'], {}, UpdateUserInput['body']>, 
   res: Response
-) => {
+) {
   try {
-    const { email } = req.body
+    const { user_id } = req.params;
+    const body = req.body;
 
-    const user = await UserService.find_user_by_email(email)
-    if (!user) return res.status(404).send('User not found')
+    const user = await UserService.findUserById(user_id);
+    if (!user) return res.status(404).send('User not found!');
 
-    const password_reset_code = await UserService.update_reset_code(user)
+    const updated = await UserService.updateUser({ _id: user_id }, { ...body });
+    if (!updated) return res.status(400).send('Failed to update user');
 
-    // send reset email link to user here
-    const link = `${BASE_URL}/reset/${user._id}/${password_reset_code}`
-    log.info(`Password reset link: ${link}`)
-
-    return res.status(200).send('Email reset link sent to user')
-  } catch (error) {
-    return res.status(500).send('Internal Server Error')
+    return res.status(200).send(updated);
+  } catch (error: any) {
+    return res.status(500).send('Internal Server Error.');
   }
 }
 
-const reset_password_handler = async (
+
+async function createAdmin(
+  req: Request<UpdateUserInput['params'], {}, UpdateUserInput['body']>, 
+  res: Response
+) {
+  try {
+    const { user_id } = req.params;
+    const { role } = req.body;
+
+    const user = await UserService.findUserById(user_id);
+    if (!user) return res.status(404).send('User not found!');
+
+    const updated = await UserService.updateUser({ _id: user_id }, { role });
+    if (!updated) return res.status(400).send('Failed to make admin');
+
+    return res.status(200).send(updated);
+  } catch (error: any) {
+    return res.status(500).send('Internal Server Error.');
+  }
+}
+
+
+async function forgotPasswordHandler(
+  req: Request<{}, {}, ResetInput['body']>, 
+  res: Response
+) {
+  try {
+    const { email } = req.body;
+
+    const user = await UserService.findByEmail(email);
+    if (!user) return res.status(404).send('User not found.');
+
+    const reset_code = generateCode();
+    user.reset_code = reset_code;
+    await user.save();
+
+    // generate password reset link 
+    const link = process.env.NODE_ENV === 'production' ? 
+      `${process.env.PROD_URL}/${user._id}/reset/${reset_code}` : `${process.env.DEV_URL}/${user._id}/reset/${reset_code}`;
+
+    log.info(`Password reset link: ${link}`);
+
+    await sendEmail({
+      to: email,
+      from: 'test@example.com',
+      subject: 'Reset Your Password.',
+      text: `Follow the link to reset your password: ${link}`,
+      html: `<b>Click <a href=${link}>here</a> to reset your password.<b/>`,
+    });
+
+    return res.status(200).send(`Password reset link sent to users' email.`);
+  } catch (error) {
+    return res.status(500).send('Internal Server Error.');
+  }
+}
+
+
+async function resetPasswordHandler(
   req: Request<ResetInput['params'], {}, ResetInput['body']>, 
   res: Response
-) => {
+) {
   try {
-    const { id, password_reset_code } = req.params
-    const { password } = req.body
+    const { id, reset_code } = req.params;
+    const { password } = req.body;
 
-    const user = await UserService.find_user_by_id(id)
-    if (!user) return res.status(404).send('User not found')
+    const user = await UserService.findUserById(id);
+    if (!user) return res.status(404).send('User not found.');
 
-    const updated = await UserService.update_password(user, password_reset_code, password)
-    if (updated) {
-      return res.status(200).send(`Users' password has been updated successfully`)
-    }
+    if (user.reset_code !== reset_code) 
+      return res.status(400).send(`Invalid password reset code.`);
 
-    return res.status(400).send(`Failed to update users' password`)
+    user.password = password;
+    user.reset_code = null;
+    await user.save();
+
+    return res.status(200).send(`Users' password has been updated successfully.`);
   } catch (error) {
-    return res.status(500).send('Internal Server Error')
+    return res.status(500).send('Internal Server Error.');
   }
 }
 
+
 export default {
-  get_users_handler, 
-  create_user_handler, 
-  forgot_password_handler, 
-  reset_password_handler,
+  forgotPasswordHandler, 
+  createUserHandler,
+  createAdmin, 
+  updateUserHandler,
+  getCurrentUserHandler, 
+  resetPasswordHandler, 
 }
--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,31 +1,24 @@
-import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken';
 
-const signJwt = (
-  object: Object, 
-  keyName: string, 
-  options?: jwt.SignOptions | undefined
-): string => {
-  const signing_key = Buffer.from(keyName, 'base64').toString('ascii')
+const signJwt = (object: Object, keyName: string, options?: jwt.SignOptions | undefined): string => {
+  const signing_key = Buffer.from(keyName, 'base64').toString('ascii');
 
   const token = jwt.sign(object, signing_key, { 
-    ...(options && options), // options - jwt time to live
-    algorithm: 'RS256'
-  })
+    ...(options && options), 
+    algorithm: 'RS256',
+  });
 
-  return token
+  return token;
 }
 
-const verifyToken = <T> (
-  token: string, 
-  verifyKey: string
-): T | null => {
-  const public_key = Buffer.from(verifyKey, 'base64').toString('ascii')
+const verifyToken = <T> (token: string, verifyKey: string): T | null => {
+  const public_key = Buffer.from(verifyKey, 'base64').toString('ascii');
 
   try {
-    const decoded = jwt.verify(token, public_key) as T
-    return decoded
+    const decoded = jwt.verify(token, public_key) as T;
+    return decoded;
   } catch(e) {
-    return null
+    return null;
   }
 }
 
