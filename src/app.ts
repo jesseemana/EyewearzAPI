b5788e863eed28369d5352260040ed4cf05f9553
Jesse Emana <jesseemana@gmail.com>
2024-04-26 13:42:34 +0200

    parsed port number to int

--- src/app.ts
+++ src/app.ts
@@ -1,72 +1,59 @@
-import 'dotenv/config'
-import express, { Response } from 'express'
-import cors from 'cors'
-import helmet from 'helmet'
-import { 
-  auth_route, 
-  user_route, 
-  booking_route,
-  product_route, 
-} from './routes'
-import cookieParser from 'cookie-parser'
-import { Database, log, swaggerDocs } from './utils'
-import { deserialize_user, error_handler }from './middleware'
+import 'dotenv/config';
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+// @ts-ignore
+import xss from 'xss-clean';
+import cookieParser from 'cookie-parser';
+import { Database, log } from './utils';
+import { deserializeUser, errorHandler }from './middleware';
+import { authRoute, userRoute, orderRoute, bookingRoute, productRoute, } from './routes';
 
-// install cross site scripting what what, maybe re-write user model in mongoose🤷🏾‍♂️
-const app = express()
-const PORT = Number(process.env.PORT)
-const database = Database.getInstance()
+const app = express();
+const database = Database.getInstance();
+const PORT = parseInt(process.env.PORT as string) || 3030;
 
 // middleware
-app.use(cors())
-app.use(helmet())
-app.use(cookieParser())
-app.use(deserialize_user)
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(xss());
+app.use(cors());
+app.use(helmet());
+app.use(cookieParser());
+app.use(deserializeUser);
+app.use(express.json({ limit: '5MB'}));
+app.use(express.urlencoded({ limit: '5MB', extended: true }));
 
-/**
- * @openapi
- * /health-check:
- *  get:
- *     tags:
- *     - Healthcheck
- *     description: Responds if the app is up and running
- *     responses:
- *       200:
- *         description: App is up and running
- */
-app.get('/health-check', (_, res: Response) => res.sendStatus(200))
-// routes
-app.use('/api/auth', auth_route)
-app.use('/api/users', user_route)
-app.use('/api/products', product_route)
-app.use('/api/reservation', booking_route)
+// check app health
+app.get('/health-check', (_req: Request, res: Response) => res.sendStatus(200));
+
+// app routes
+app.use('/api/v1/auth', authRoute);
+app.use('/api/v1/products', productRoute);
+app.use('/api/v1/orders', orderRoute);
+app.use('/api/v1/user', userRoute);
+app.use('/api/v1/reservation', bookingRoute);
+
 // error handling middleware
-app.use(error_handler)
+app.use(errorHandler);
 
-function start_server() {
+function startServer() {
   const server = app.listen(PORT, () => {
-    log.info(`App listening at: http://localhost:${PORT}`)
-    database.connect()
-    swaggerDocs(app, PORT)
+    database.connect();
+    log.info(`Server listening at: http://localhost:${PORT}`);
   })
 
-  const signals = ['SIGTERM', 'SIGINT']
+  const signals = ['SIGTERM', 'SIGINT'];
 
   const gracefulShutdown = (signal: string) => {
     process.on(signal, () => {
-      log.info(`Received signal: ${signal}, shutting down...`)
-      server.close()
-      database.disconnect()
-      log.info('Goodbye...')
-      process.exit(0)
+      log.info(`Received signal: ${signal}, shutting down...`);
+      server.close();
+      database.disconnect();
+      log.info('Goodbye...');
+      process.exit(0);
     })
   }
 
-  for (let i=0; i < signals.length; i++) {
-    gracefulShutdown(signals[i])
-  }
+  signals.forEach((_signal, index) => gracefulShutdown(signals[index]));
 }
 
-start_server()
+startServer();
