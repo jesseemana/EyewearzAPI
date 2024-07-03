854f20deeb137cfe588589573f9827606683445b
Jesse Emana <jesseemana@gmail.com>
2024-07-03 21:17:27 +0200

    minor changes

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,14 +2,12 @@ import log from './logger';
 import { signJwt, verifyToken } from './jwt';
 import { Database } from './database';
 import { sendEmail } from './mailer';
-import { generateCode } from './generate-code';
 import uploadPicture from './upload-picture';
 
 export {
   log, 
   Database, 
   sendEmail,
-  generateCode,
   verifyToken, 
   signJwt, 
   uploadPicture,
