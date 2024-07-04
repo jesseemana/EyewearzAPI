7085cb3b4d0268ddc49cc0e69d2a1428f75a4497
Jesse Emana <jesseemana@gmail.com>
2024-07-04 23:21:45 +0200

    removed reservation controller

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,12 +2,10 @@ import AuthController from './auth.controller'
 import UserController from './user.controller'
 import OrderController from './order.controller'
 import ProductController from './product.controller'
-import BookingController from './reservation.controller'
 
 export {
+  OrderController,
   AuthController,
   UserController,
-  OrderController,
   ProductController,
-  BookingController,
 }
