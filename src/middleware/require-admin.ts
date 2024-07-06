ee78810dee9e9ada923022b51b496aaf61081dc7
Jesse Emana <jesseemana@gmail.com>
2024-07-06 23:47:42 +0200

    minor changes

--- src/middleware/require-admin.ts
+++ src/middleware/require-admin.ts
@@ -4,7 +4,9 @@ const requireAdmin = (req: Request, res: Response, next: NextFunction) => {
   const user = req.user
   if (!user) return res.sendStatus(403)
   if (!user.admin) {
-    return res.status(401).json({ msg: 'Only admins can make this operation' })
+    return res.status(401).json({ 
+      msg: 'Only admins can make this operation' 
+    })
   }
   next()
 }
